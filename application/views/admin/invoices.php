356240182b331b0d4b1f6905df71292825ceee3c
unknown <taulant.sfishta@gmail.com>
2025-05-08 16:33:27 +0200

    ndryshime ne invoice

--- application/views/admin/invoices.php
+++ application/views/admin/invoices.php
@@ -603,7 +603,6 @@ $('#search_results_table th').click(function() {
             }
             return 0;
         });
-        console.log('123');
 
         $.each(rows, function(index, row) {
             table.children('tbody').append(row);
